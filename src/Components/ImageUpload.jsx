b5719685a46959d4a7477556c49608bd01c38b3c
Sai Prakash <Saiprakashreddy1409@gmail.com>
2020-01-17 16:11:46 +0530

    Working With Url Submitting and Download

--- src/Components/ImageUpload.jsx
+++ src/Components/ImageUpload.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase, { storage } from '../config/Fire';
-import Login from '../Login';
+import '../Logform.css';
+
 
 class ImageUpload extends Component {
   constructor(props) {
@@ -10,57 +11,59 @@ class ImageUpload extends Component {
       url: '',
       progress: 0
     }
-    // console.log(Login.state.email);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
+    this.handleChange = this
+      .handleChange
+      .bind(this);
+      this.handleUpload = this.handleUpload.bind(this);
   }
   handleChange = e => {
     if (e.target.files[0]) {
       const image = e.target.files[0];
-      this.setState(() => ({ image }));
+      this.setState(() => ({image}));
     }
   }
   handleUpload = () => {
-    const { image } = this.state;
-    const fireuser = firebase.auth().currentUser.email;
-    const uploadTask = storage.ref(`${fireuser}/${image.name}`).put(image);
-    uploadTask.on('state_changed',
+      const {image} = this.state;
+      const fireuser = firebase.auth().currentUser.email;
+      const uploadTask = storage.ref(`${fireuser}/${image.name}`).put(image);
+            uploadTask.on('state_changed', 
       (snapshot) => {
         // progrss function ....
         const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-        this.setState({ progress });
-      },
+        this.setState({progress});
+      }, 
       (error) => {
-        // error function ....
+           // error function ....
         console.log(error);
-      },
-      () => {
-        // complete function ....
-        storage.ref('images').child(image.name).getDownloadURL().then(url => {
-          console.log(url);
-          storage.ref(`Url/${image.name}`).put(url);
-
+      }, 
+    () => {
+      storage.ref(`${fireuser}/${image.name}`).getDownloadURL().then(url => {
+            console.log("Url Here : ");
+            console.log(url);
+            this.setState({url});
         })
-      });
+
+    });
   }
   render() {
     const style = {
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center'
+ 
     };
     return (
-      <div style={style}>
-        <progress value={this.state.progress} max="100" />
-        <br />
+      <div style={style}>        
         <input type="file" onChange={this.handleChange} />
-        <button onClick={this.handleUpload}>Upload</button>
-        <br />
-        <img src={this.state.url} height="300" width="400" />
+            
+        <button onClick={this.handleUpload} className="btn btn-primary" >Upload</button>
+        <div>
+          <br/>
+        <h6>Progress is : {this.state.progress} </h6>
+        <progress value={this.state.progress} max="100" />
+        </div>
+        <img src={this.state.url } height="300" width="500"/>
+        <a href={this.state.url}>Download</a>
+
       </div>
-    )
+  )
   }
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import fire from './config/Fire';
+import './Logform.css';
 
 class Login extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class Login extends Component {
       password: ''
     };
   }
-  
+
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -21,37 +21,58 @@ class Login extends Component {
 
   login(e) {
     e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
     }).catch((error) => {
-        console.log(error);
-      });
+      console.log(error);
+    });
   }
 
-  signup(e){
+  signup(e) {
     e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+    }).then((u) => { console.log(u) })
+      .catch((error) => {
         console.log(error);
       })
   }
   render() {
     return (
-      <div className="col-md-6">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input  value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input  value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-          </div>
-          <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
-          <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
-        </form>
-      
+      <div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">KAILI</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+       
+
+        <div className="col-md-5" id="logform">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
+            <button onClick={this.signup} style={{ marginLeft: '25px' }} className="btn btn-success">Signup</button>
+          </form>
+        </div>
       </div>
     );
   }
